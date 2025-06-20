17f2fccfa0cee6229b0a036223773dbee3f7b164
haydenlchapman <haydenchapman1794@gmail.com>
2025-06-19 23:06:55 -0400

    Readded root parameter to MainPage's constructor, created a title for my bookstore, and explicitly created my own root, which I passed to my MainPage instance in a different fashion than before.

--- main_page.py
+++ main_page.py
@@ -1,19 +1,19 @@
 import tkinter as tk
 
 class MainPage(tk.Frame):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, root=None):
+        super().__init__(root)
         self.grid()
         self.create_widgets()
 
     def create_widgets(self):
-        self.media_label = tk.Label(self, text="Main Page")
+        self.media_label = tk.Label(self, text="Flourishing Blotts")
         self.media_label.config(bg="#00ffff")
         self.media_label.grid()
         self.quit_button = tk.Button(self, text="Quit", command=self.quit)
         self.quit_button.grid()
 
-main_page = MainPage()
-main_page.root = tk.Tk()
-main_page.root.title("Main Page")
+root = tk.Tk()
+root.title("Flourishing Blotts")
+main_page = MainPage(root)
 main_page.mainloop()
