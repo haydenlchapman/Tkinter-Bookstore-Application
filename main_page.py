06d8ad58fce93fd62bc8e9385557e3f084f19667
haydenlchapman <haydenchapman1794@gmail.com>
2025-06-19 23:35:09 -0400

    Configured application to be fullscreen, started configuring title's position on page. Updated some variable names to be more descriptive.

--- main_page.py
+++ main_page.py
@@ -7,13 +7,20 @@ class MainPage(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.media_label = tk.Label(self, text="Flourishing Blotts")
-        self.media_label.config(bg="#00ffff")
-        self.media_label.grid()
+        self.page_title = tk.Label(self, text="Flourishing Blotts", font=("Cambria", 40))
+        self.page_title.config(bg="#00ffff")
+        self.page_title.grid(row=0, column=2, pady=(100, 20))
         self.quit_button = tk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.grid()
+        self.quit_button.grid(row=1, column=2)
 
+# Define root
 root = tk.Tk()
 root.title("Flourishing Blotts")
+root.attributes("-fullscreen", True)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+
+# Instantiate and run our main page
 main_page = MainPage(root)
 main_page.mainloop()
