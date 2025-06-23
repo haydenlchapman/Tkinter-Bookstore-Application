90c0746f100a10d31c099959cf3bb24644e7b8a0
haydenlchapman <haydenchapman1794@gmail.com>
2025-06-23 02:25:47 -0400

    Created constant for background color.

--- main_page.py
+++ main_page.py
@@ -1,17 +1,17 @@
 import tkinter as tk
-from gc import collect
 
+BACKGROUND_COLOR = "#f5f5dc" # Beige
 
 class MainPage(tk.Frame):
     def __init__(self, root=None):
         super().__init__(root)
-        self.configure(bg="#f5f5dc") # Assign window beige color
+        self.configure(bg=BACKGROUND_COLOR) # Assign window beige color
         self.grid()
         self.create_widgets()
 
     def create_widgets(self):
         # Create, style, and place exit button
-        tk.Button(self, text="Quit", font=("Cambria, 15"), command=self.quit).grid(row=0, column=0, sticky="e", padx=10, pady=(20, 20))
+        tk.Button(self, text="Quit", font=("Cambria, 15"), command=self.quit).grid(pady=(20, 20))
 
         # Create and position page title
         self.page_title = tk.Label(self, text="Flourishing Blotts", font=("Cambria", 40))
@@ -22,7 +22,7 @@ class MainPage(tk.Frame):
 # Define root window
 root = tk.Tk()
 root.title("Flourishing Blotts")
-root.configure(bg="#f5f5dc") # Assign root beige color
-root.attributes("-fullscreen", True) # Enable fullscreen3
+root.configure(bg=BACKGROUND_COLOR) # Assign root beige color
+root.attributes("-fullscreen", True) # Enable fullscreen
 
 main_page = MainPage(root).mainloop() # Instantiate and run main page
