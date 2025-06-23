63e0630e468cf2c3723a37db5ad0f834d640e0e9
haydenlchapman <haydenchapman1794@gmail.com>
2025-06-23 04:16:18 -0400

    Removed main_page variable (for now, at least).

--- main_page.py
+++ main_page.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 
+# Constants
 BACKGROUND_COLOR = "#f5f5dc" # Beige
 BOOKSTORE_TITLE = "Flourishing Blotts"
+#consider making const var for cambria
 
 class MainPage(tk.Frame):
     def __init__(self, root=None):
@@ -12,12 +14,12 @@ class MainPage(tk.Frame):
 
     def create_widgets(self):
         # Create, style, and place exit button
-        tk.Button(self, text="Quit", font=("Cambria, 15"), command=self.quit).grid(pady=(20, 20))
+        tk.Button(self, text="Quit", font=("Cambria", 15), command=self.quit).grid(row=0, column=2, pady=(20, 20), padx=20)
 
         # Create and position page title
         self.page_title = tk.Label(self, text=BOOKSTORE_TITLE, font=("Cambria", 40))
         self.page_title.config(bg="#00ffff")
-        self.page_title.grid(pady=(100, 20))
+        self.page_title.grid(row=1, column=1, pady=(100, 20), padx=20)
 
 
 # Define root window
@@ -26,4 +28,4 @@ root.title(BOOKSTORE_TITLE)
 root.configure(bg=BACKGROUND_COLOR) # Assign root beige color
 root.attributes("-fullscreen", True) # Enable fullscreen
 
-main_page = MainPage(root).mainloop() # Instantiate and run main page
+MainPage(root).mainloop() # Instantiate and run main page
