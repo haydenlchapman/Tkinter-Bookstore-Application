6cfdc81242f935b9845afa309e6c76f27c79b888
haydenlchapman <haydenchapman1794@gmail.com>
2025-06-23 02:27:24 -0400

    Created constant variable for boosktore title.

--- main_page.py
+++ main_page.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 BACKGROUND_COLOR = "#f5f5dc" # Beige
+BOOKSTORE_TITLE = "Flourishing Blotts"
 
 class MainPage(tk.Frame):
     def __init__(self, root=None):
@@ -14,14 +15,14 @@ class MainPage(tk.Frame):
         tk.Button(self, text="Quit", font=("Cambria, 15"), command=self.quit).grid(pady=(20, 20))
 
         # Create and position page title
-        self.page_title = tk.Label(self, text="Flourishing Blotts", font=("Cambria", 40))
+        self.page_title = tk.Label(self, text=BOOKSTORE_TITLE, font=("Cambria", 40))
         self.page_title.config(bg="#00ffff")
         self.page_title.grid(pady=(100, 20))
 
 
 # Define root window
 root = tk.Tk()
-root.title("Flourishing Blotts")
+root.title(BOOKSTORE_TITLE)
 root.configure(bg=BACKGROUND_COLOR) # Assign root beige color
 root.attributes("-fullscreen", True) # Enable fullscreen
 
