344d7de802a92ac17d60ddc411600bd335f748a6
haydenlchapman <haydenchapman1794@gmail.com>
2025-07-05 16:27:13 -0400

    Added page title as a parameter to MainPage, instead of making create_widgets, a helper function, responsible for receiving class parameters.

--- main_page.py
+++ main_page.py
@@ -6,13 +6,13 @@ BOOKSTORE_TITLE = "Flourishing Blotts"
 RIGHT = tk.RIGHT
 
 class MainPage(tk.Frame):
-    def __init__(self, root=None, background_color=BACKGROUND_COLOR, font_name="Cambria"):
+    def __init__(self, root=None, background_color=BACKGROUND_COLOR, font_name="Cambria", page_title=BOOKSTORE_TITLE):
         super().__init__(root)
         self.configure(bg=background_color) # Assign window beige color
         self.pack()
-        self.create_widgets(background_color, font_name)
+        self.create_widgets(background_color, font_name, page_title)
 
-    def create_widgets(self, background_color, font_name, bookstore_title=BOOKSTORE_TITLE):
+    def create_widgets(self, background_color, font_name, bookstore_title):
         # Create, style, and place exit button
         tk.Button(self, text="Quit", font=(font_name, 15), command=self.quit).pack(side=RIGHT, padx=20, pady=20)
 
