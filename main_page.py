25b0a6915207c0b8151e9078148b97f0e97cc63c
haydenlchapman <haydenchapman1794@gmail.com>
2025-07-06 19:52:05 -0400

    Replaced string literals with tk constants where appropriate.

--- main_page.py
+++ main_page.py
@@ -8,12 +8,12 @@ class MainPage(tk.Frame):
     def __init__(self, root=None, background_color=BACKGROUND_COLOR, font_name="Cambria", page_title=BOOKSTORE_TITLE):
         super().__init__(root) # Create our frame in the root window
         self.configure(bg=background_color)
-        self.pack(fill="x")
+        self.pack(fill=tk.X)
         self.create_widgets(background_color, font_name, page_title)
 
     def create_widgets(self, background_color, font_name, page_title):
         # Create, style, and pack quit button
-        tk.Button(self, text="Quit", font=(font_name, 15), command=self.quit).pack(padx=40, pady=25, anchor="ne")
+        tk.Button(self, text="Quit", font=(font_name, 15), command=self.quit).pack(padx=40, pady=25, anchor=tk.E)
 
         # Create and position page title
         self.page_title = tk.Label(self, text=page_title, font=(font_name, 40))
